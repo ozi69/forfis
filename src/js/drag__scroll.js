e9ec8d35b778e473e46ca76aeb32d44c736276b1
Batura Daniel <d.batura99@gmail.com>
2026-01-12 16:48:19 +0700

    init

---
+++ src/js/drag__scroll.js
@@ -0,0 +1,56 @@
+document.addEventListener('DOMContentLoaded', function() {
+  class DragScroll {
+    constructor(container) {
+      this.container = container;
+      this.isDown = false;
+      this.startX = 0;
+      this.scrollLeft = 0;
+      
+      this.init();
+    }
+
+    // если в консоли возникает ошибка, то она связана с ненахожденим контейнар в котором проихсодит свайп
+    
+    init() {
+      this.container.addEventListener('mousedown', this.onMouseDown.bind(this));
+      this.container.addEventListener('mouseleave', this.onMouseLeave.bind(this));
+      this.container.addEventListener('mouseup', this.onMouseUp.bind(this));
+      this.container.addEventListener('mousemove', this.onMouseMove.bind(this));
+    }
+    
+    onMouseDown(e) {
+      this.isDown = true;
+      this.container.classList.add('dragging');
+      this.startX = e.pageX - this.container.offsetLeft;
+      this.scrollLeft = this.container.scrollLeft;
+    }
+    
+    onMouseLeave() {
+      this.isDown = false;
+      this.container.classList.remove('dragging');
+    }
+    
+    onMouseUp() {
+      this.isDown = false;
+      this.container.classList.remove('dragging');
+    }
+    
+    onMouseMove(e) {
+      if (!this.isDown) return;
+      e.preventDefault();
+      const x = e.pageX - this.container.offsetLeft;
+      const walk = (x - this.startX) * 2;
+      this.container.scrollLeft = this.scrollLeft - walk;
+    }
+  }
+
+  // Использование
+  const commentContainer = new DragScroll(document.querySelector('.container-comments'));
+  const insightsContainer = new DragScroll(document.querySelector('.container-insights'));
+  const careersContainer = new DragScroll(document.querySelector('.container-careers'));
+
+  // Или для всех элементов с классом .drag-scroll
+  document.querySelectorAll('.drag-scroll').forEach(container => {
+    new DragScroll(container);
+  });
+});
